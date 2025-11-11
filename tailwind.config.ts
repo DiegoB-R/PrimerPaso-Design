aa7485b202d1f011ca3555a1209942d9ef34937e
Eroroshy <eroroshy666@gmail.com>
2025-11-11 14:28:07 -0500

    Commit inicial a la rama de Nuxt
    -Modificaciones al template de Landing Page y pruebas

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './app.vue',
+    './plugins/**/*.{js,ts}',
+    './error.vue'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+} satisfies Config
